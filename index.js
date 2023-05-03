7611b29f6a87547d28ef3e280fb3ad4189f1b07b
FedeCrossetto <fede.vazquez.crossetto@gmail.com>
2023-05-03 16:28:22 -0300

    Add logic BFF

--- index.js
+++ index.js
@@ -1,45 +1,45 @@
-const express = require('express')
-const cors = require('cors')
-const axios = require('axios')
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
 
+const app = express();
+const port = 3000;
 
-const app = express()
-const port = 3000
-
-let url = 'https://echo-serv.tbxnet.com/v1/secret/files'
-//#region
-app.use(
-    express.urlencoded({
-        extended: true
-    }))
-
-app.use(
-    express.json({
-        type: "*/*"
-    }))
+//middlewares ---------------------
+app.use(express.json());
+app.use(express.json({ type: "*/*" }));
 app.use(cors());
-//#endregion
 
+//enviroment variables ---------------------
+let baseurl = "https://echo-serv.tbxnet.com/v1/secret";
 let config = {
-    headers:{
-        Authorization: "Bearer aSuperSecretKey"
-    }
-}
-// app.get('/prueba',(req,res)=>{
-//     res.send('Hola estoy funcionando');
-// })
+  headers: {
+    Authorization: "Bearer aSuperSecretKey",
+  },
+};
 
-// axios.get(url,config).then(response =>{
-//     console.log(response.data)
-// })
+//routes---------------------
+//Get list files
+app.get(`/files/data`, (req, res) => {
+  axios.get(`${baseurl}/files`, config).then((response) => {
+    console.log(response.data);
+    res.send(JSON.stringify(response.data));
+  });
+});
 
-app.get('/test',(req,res)=>{
-    axios.get(url,config).then(response =>{
-        console.log(response.data)
-    })
-})
+//Get detail file.
+app.get(`/files/data2`, (req, res) => {
+  axios.get(`${baseurl}/file/test9.csv`, config).then((response) => {
+    const dataArray = response.data.split("\n").map((line) => {
+      const [file, text, number, hex, _] = line.split(",");
+      return { file, text, number, hex };
+    });
+    res.send(JSON.stringify(dataArray));
+  });
+});
 
 
-app.listen(port, ()=>{
-    console.log(`Estoy ejecuntandome en http://localtosst:${port}`)
-})
+//server running ---------------------
+app.listen(port, () => {
+  console.log(`Server running on port http://localhost:${port}`);
+});
